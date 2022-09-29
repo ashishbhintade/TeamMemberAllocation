b6a217b061bfc6f52d51a8642e5c5843744c3621
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2022-09-29 10:17:54 +0000

    Code organization

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     <header className="container">
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-8">
-          <h1 class="text-danger">404 - Page not found</h1>
+          <h1 className="text-danger">404 - Page not found</h1>
         </div>
       </div>
     </header>
